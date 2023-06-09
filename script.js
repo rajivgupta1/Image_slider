59dca0f2b93da481982860efbaf523b5c6f38825
Rajiv Kumar Gupta <100814533+rajivgupta1@users.noreply.github.com>
2023-06-09 14:18:38 +1000

    hello

---
+++ script.js
@@ -0,0 +1,19 @@
+
+
+
+const swiper = new Swiper('.swiper', {
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+  });
+
